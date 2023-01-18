37b0fd22a15cd414134092a2b520161763147706
Dmutriy <105307970+Dmutriy@users.noreply.github.com>
2023-01-18 18:11:15 +0200

    Update contactsSlice.js

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, nanoid } from '@reduxjs/toolkit';
 
 const contactsInitialState = {
-  contacts: [
+  items: [
     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
@@ -15,7 +15,7 @@ const contactsSlice = createSlice({
   reducers: {
     addContact: {
       reducer: (state, action) => {
-        return { ...state, contacts: [...state.contacts, action.payload] };
+        state.items = [...state.items, action.payload];
       },
       prepare(newContact) {
         return {
@@ -27,12 +27,9 @@ const contactsSlice = createSlice({
       },
     },
     deleteContact(state, action) {
-      return {
-        ...state,
-        contacts: state.contacts.filter(
-          contact => contact.id !== action.payload
-        ),
-      };
+      state.items = state.items.filter(
+        contact => contact.id !== action.payload
+      );
     },
   },
 });
