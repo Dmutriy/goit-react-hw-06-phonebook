5aea6a98894b7e89d319ea9677d3e31d8504cdc0
Dmutriy <105307970+Dmutriy@users.noreply.github.com>
2023-01-18 18:11:10 +0200

    del bin

--- src/redux/filtersSlice.js
+++ src/redux/filtersSlice.js
@@ -12,23 +12,3 @@ export const filterSlice = createSlice({
 
 export const { filterChange } = filterSlice.actions;
 export const filterReducer = filterSlice.reducer;
-
-// import { createSlice } from '@reduxjs/toolkit';
-// import { statusFilters } from './constants';
-
-// const filtersInitialState = {
-//   status: statusFilters.all,
-// };
-
-// const filtersSlice = createSlice({
-//   name: 'filters',
-//   initialState: filtersInitialState,
-//   reducers: {
-//     setStatusFilter(state, action) {
-//       state.status = action.payload;
-//     },
-//   },
-// });
-
-// export const { setStatusFilter } = filtersSlice.actions;
-// export const filtersReducer = filtersSlice.reducer;
